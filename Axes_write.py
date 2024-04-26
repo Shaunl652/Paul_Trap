829a08b0062acb7825500234fcbc4ee95a64c9e8
Shaun <shaunl652@gmail.com>
2024-04-26 11:43:41 +0100

    Corrected for this branch

--- Axes_write.py
+++ Axes_write.py
@@ -15,22 +15,22 @@ def valid(line):
 		return True
 with open('dims.txt','r') as file:
 	vals = [line for line in file if valid(line)]
-r0 = float(vals[0])*1e3
-z0 = float(vals[1])*1e3
-
+x0 = float(vals[0])*1e3
+y0 = float(vals[1])*1e3
+z0 = float(vals[2])*1e3
 
 # Measurment locations
-xaxis = np.linspace(-r0,+r0,11)/10
-yaxis = np.linspace(-r0,+r0,11)/10
+xaxis = np.linspace(-x0,+x0,11)/10
+yaxis = np.linspace(-y0,+y0,11)/10
 zaxis = np.linspace(-z0,+z0,11)/10
 
-with open('x_axis.dat','w') as f:
-    for x in xaxis:
-        f.write(f'{x} 0 0 \n')
+with open('u_axis.dat','w') as f:
+    for x,y in zip(xaxis,yaxis):
+        f.write(f'{-x} {+y} 0 \n')
 
-with open('y_axis.dat','w') as f:
-    for y in yaxis:
-        f.write(f'0 {y} 0 \n')
+with open('v_axis.dat','w') as f:
+    for x,y in zip(xaxis,yaxis):
+        f.write(f'{+x} {+y} 0 \n')
 
 with open('z_axis.dat','w') as f:
     for z in zaxis:
