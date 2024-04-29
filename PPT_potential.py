3993ed59f6cf807c41490dfbf77ca4275c9bc7e5
Shaun <shaunl652@gmail.com>
2024-04-29 13:47:30 +0100

    Made some progress towards getting ths right

--- PPT_potential.py
+++ PPT_potential.py
@@ -9,54 +9,63 @@
 
 import numpy as np
 from numpy import sqrt,trapz 
+from scipy.constants import elementary_charge,pi
 
-def psi(z,epsilon,a,b):
-    """
-    Finds the potential at the point z
+# Set up properties
+Q = 86*elementary_charge
+R = 150e-9
+density = 2329
+Mass = (4/3)*pi*density*R**3
+Omega = 2*pi*4e3
+Vin = 0 
+Vout = 340
+e = Vin/Vout
+a = 1.07e-3
+b = 3.63e-3
 
-    Parameters
-    ----------
-    z : Array or float
-        z position.
-    epsilon : FLAOT
-        Ratio of the inner volatge over the outer voltage.
-    a : Float
-        radius of inner ring in m.
-    b : FLAOT
-        Radius of outer ring in m.
+z_vals = np.linspace(0.01,3,101)*1e-3
 
-    Returns
-    -------
-    Array or float
-        The potential at the z location(s).
+def psi(z):
 
-    """
     
-
-
+    epsilon =Vin/Vout
+    A = Q**2*Vout**2/(4*Mass*Omega**2)
     # This is the differentiation from Alda EQ2 givein by mathematica
     term1 = b**2/((1+(b/z)**2)**(3/2)*z**3)
     term2 = ((1-epsilon)*a**2)/((1+(a/z)**2)**(3/2)*z**3)
     
     Dkappa = term1 + term2
     
-    return np.abs(Dkappa)**2
+    return A*np.abs(Dkappa)**2
 
-def z0(a,b,e):
+  
+z0numer = b**2 * a**(4/3) * (1-e)**(2/3) - a**2 * b**(4/3)
+z0denom = b**(4/3) - a**(4/3) * (1-e)**(2/3)
     
-    numer = b**2 * a**(4/3) * (1-e)**(2/3) - a**2 * b**(4/3)
-    denom = b**(4/3) - a**(4/3) * (1-e)**(2/3)
-    
-    return sqrt(numer/denom)
+z0 =  sqrt(z0numer/z0denom)
 
 
-def zmax(a,b,e):
+zmaxnumer = b**2 * a**(4/5) * (1-e)**(2/5) - a**2 * b**(4/5)
+zmaxdenom = b**(4/5) - a**(4/5) * (1-e)**(2/5)
     
-    numer = b**2 * a**(4/5) * (1-e)**(2/5) - a**2 * b**(4/5)
-    denom = b**(4/5) - a**(4/5) * (1-e)**(2/5)
-    
-    return sqrt(numer/denom)
+zmax = sqrt(zmaxnumer/zmaxdenom)
 
-def Trap_Depth(a,b,e):
-    
-    return psi(zmax(a,b,e),e,a,b) - psi(z0(a,b,e),e,a,b)
+Trap_Depth = psi(zmax) - psi(z0)
+
+
+
+psi_vals = psi(z_vals)
+# Begin plotting
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+ax.plot(z_vals/1e-3,psi_vals)
+ax.set(xlabel='x [mm]',ylabel='$\\psi$')
+
+print(f'Trap Depth = {Trap_Depth:.2e}')
+
+
+
+
+
