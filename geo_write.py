f5aa89f2d31573eb02b9bdfb0996554f56fcac8c
Shaun <shaunl652@gmail.com>
2024-05-01 12:00:54 +0100

    New gewometry

--- geo_write.py
+++ geo_write.py
@@ -22,36 +22,35 @@ with open('dims.txt','r') as file:
 r0 = float(vals[0])*1e3
 z0 = float(vals[1])*1e3
 
-# Distance from surface of the rod to its centre
-rod_rad = 0.635e3
+
+Blade_Length = 22e3/2 # Blade is 22mm long
 ECP_thick = 0.1e3
 
 with open('TrapAC.scuffgeo','w') as f:
-    f.write(f'OBJECT Rod1\n   MESHFILE Rod.o.msh\n   DISPLACED {+(r0+rod_rad)} 0 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod1\n   MESHFILE Rod.o.msh\n   DISPLACED {+(r0+Blade_Length)} 0 0\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod2\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {+(r0+rod_rad)} 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod2\n   MESHFILE Rod.o.msh\n   ROTATED 90 ABOUT 0 0 1\n   DISPLACED 0 {+(r0+Blade_Length)} 0\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod3\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {-(r0+rod_rad)}  0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod3\n   MESHFILE Rod.o.msh\n   ROTATED 90 ABOUT 0 0 1\n   DISPLACED 0 {-(r0+Blade_Length)}  0\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod4\n   MESHFILE Rod.o.msh\n   DISPLACED {-(r0+rod_rad)} 0 0\nENDOBJECT\n')
-    f.write('\n')
-    f.write('\n')
-    f.write(f'OBJECT End_Cap_Top\n   MESHFILE Endcap.o.msh\n   DISPLACED 0 0 {+(z0+ECP_thick/2)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
-    f.write('\n')
-    f.write(f'OBJECT End_Cap_Bot\n   MESHFILE Endcap.o.msh\n   DISPLACED 0 0 {-(z0+ECP_thick/2)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
-    
-with open('TrapDC.scuffgeo','w') as f:
-    f.write(f'OBJECT Rod1\n   MESHFILE Rod.o.msh\n   DISPLACED {+(r0+rod_rad)} 0 0\nENDOBJECT\n')
-    f.write('\n')
-    f.write(f'OBJECT Rod2\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {+(r0+rod_rad)} 0\nENDOBJECT\n')
-    f.write('\n')
-    f.write(f'OBJECT Rod3\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {-(r0+rod_rad)}  0\nENDOBJECT\n')
-    f.write('\n')
-    f.write(f'OBJECT Rod4\n   MESHFILE Rod.o.msh\n   DISPLACED {-(r0+rod_rad)} 0 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod4\n   MESHFILE Rod.o.msh\n   DISPLACED {-(r0+Blade_Length)} 0 0\nENDOBJECT\n')
     f.write('\n')
     f.write('\n')
     f.write(f'OBJECT End_Cap_Top\n   MESHFILE Endcap.o.msh\n   DISPLACED 0 0 {+(z0+ECP_thick/2)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     f.write('\n')
     f.write(f'OBJECT End_Cap_Bot\n   MESHFILE Endcap.o.msh\n   DISPLACED 0 0 {-(z0+ECP_thick/2)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     
+with open('TrapAC.scuffgeo','w') as f:
+    f.write(f'OBJECT Rod1\n   MESHFILE Rod.o.msh\n   DISPLACED {+(r0+Blade_Length)} 0 0\nENDOBJECT\n')
+    f.write('\n')
+    f.write(f'OBJECT Rod2\n   MESHFILE Rod.o.msh\n   ROTATED 90 ABOUT 0 0 1\n   DISPLACED 0 {+(r0+Blade_Length)} 0\nENDOBJECT\n')
+    f.write('\n')
+    f.write(f'OBJECT Rod3\n   MESHFILE Rod.o.msh\n   ROTATED 90 ABOUT 0 0 1\n   DISPLACED 0 {-(r0+Blade_Length)}  0\nENDOBJECT\n')
+    f.write('\n')
+    f.write(f'OBJECT Rod4\n   MESHFILE Rod.o.msh\n   DISPLACED {-(r0+Blade_Length)} 0 0\nENDOBJECT\n')
+    f.write('\n')
+    f.write('\n')
+    f.write(f'OBJECT End_Cap_Top\n   MESHFILE Endcap.o.msh\n   DISPLACED 0 0 {+(z0+ECP_thick/2)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
+    f.write('\n')
+    f.write(f'OBJECT End_Cap_Bot\n   MESHFILE Endcap.o.msh\n   DISPLACED 0 0 {-(z0+ECP_thick/2)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     
