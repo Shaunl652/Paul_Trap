66d3b947e8b2ee16cf48bc47997780f5a6f1a8d6
Shaun <shaunl652@gmail.com>
2024-05-23 14:51:28 +0100

    Fixed all the isses and extended
    Fixed the isue in the differential
    Included PPT1 and PPT2 from Alda 2016
    Included values for pollen

--- PPT_potential.py
+++ PPT_potential.py
@@ -12,12 +12,12 @@ from numpy import sqrt,trapz
 from scipy.constants import elementary_charge,pi
 
 # Set up properties
-Q = 86*elementary_charge
-R = 150e-9
-density = 2329
+Q = 86*elementary_charge#0.33e-15#
+R = 150e-9#15e-6#
+density = 2329#1000#
 Mass = (4/3)*pi*density*R**3
 Omega = 2*pi*4e3
-Vin = 0 
+Vin = 0
 Vout = 340
 e = Vin/Vout
 a = 1.07e-3
@@ -34,7 +34,7 @@ def psi(z):
     term1 = b**2/((1+(b/z)**2)**(3/2)*z**3)
     term2 = ((1-epsilon)*a**2)/((1+(a/z)**2)**(3/2)*z**3)
     
-    Dkappa = term1 + term2
+    Dkappa = term2 - term1
     
     return A*np.abs(Dkappa)**2
 
@@ -52,20 +52,62 @@ zmax = sqrt(zmaxnumer/zmaxdenom)
 
 Trap_Depth = psi(zmax) - psi(z0)
 
+def f(a,b):
+    numer = 9*(b**(2/3)-a**(2/3))**2 *(b**(2/3)+a**(2/3))**6
+    denom = b**(4/3)*a**(4/3)*(b**(4/3) +b**(2/3)*a**(2/3) + a**(4/3))**5
+    return sqrt(numer/denom)
 
+def qval(Q,V,M,Omega,a,b):
+    return ((2*Q*V)/(M*Omega**2)) * f(a,b)
 
-psi_vals = psi(z_vals)
+psi_vals = psi(z_vals)/1e-17
+print(f'PPT1 stability q = {qval(Q,Vout,Mass,Omega,a,b):.2f}')
 # Begin plotting
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
 
-ax.plot(z_vals/1e-3,psi_vals)
+ax.plot(z_vals/1e-3,psi_vals,color='k',label='PPT1')
 ax.set(xlabel='x [mm]',ylabel='$\\psi$')
+ax.text(z0/1e-3,psi_vals.max()/200,'$z_0$',fontsize=12)
+ax.scatter(z0/1e-3,psi(z0)/1e-17,marker='x',color='k')
+ax.text(zmax/1e-3,psi(zmax)/1e-17+psi_vals.max()/200,'$z_{max}$',fontsize=12)
+ax.scatter(zmax/1e-3,psi(zmax)/1e-17,marker='x',color='k')
 
-print(f'Trap Depth = {Trap_Depth:.2e}')
+# =============================================================================
+# print(f'Trap Depth = {Trap_Depth:.2e}')
+# print(f'z0 = {z0/1e-3:.2f} mm')
+# print(f'zmax = {zmax/1e-3:.2f} mm')
+# 
+# =============================================================================
+
+a = 0.36e-3
+b = 1.17e-3
+
+z0numer = b**2 * a**(4/3) * (1-e)**(2/3) - a**2 * b**(4/3)
+z0denom = b**(4/3) - a**(4/3) * (1-e)**(2/3)
+    
+z0 =  sqrt(z0numer/z0denom)
 
 
+zmaxnumer = b**2 * a**(4/5) * (1-e)**(2/5) - a**2 * b**(4/5)
+zmaxdenom = b**(4/5) - a**(4/5) * (1-e)**(2/5)
+    
+zmax = sqrt(zmaxnumer/zmaxdenom)
 
+Trap_Depth = psi(zmax) - psi(z0)
 
+print(f'PPT2 stability q = {qval(Q,Vout,Mass,Omega,a,b):.2f}')
 
+psi_vals = psi(z_vals)/1e-17
+
+ax.plot(z_vals/1e-3,psi_vals,color='r',label='PPT2')
+ax.set(xlabel='x [mm]',ylabel='$\\psi$')
+ax.text(z0/1e-3,psi_vals.max()/100,'$z_0$',fontsize=12,color='r')
+ax.scatter(z0/1e-3,psi(z0)/1e-17,marker='x',color='r')
+ax.text(zmax/1e-3,psi(zmax)/1e-17+psi_vals.max()/500,'$z_{max}$',fontsize=12,color='r')
+ax.scatter(zmax/1e-3,psi(zmax)/1e-17,marker='x',color='r')
+
+ax.set_ylim(0,5)
+
+ax.legend()
