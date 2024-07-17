c5ade8a142d7d16ff063aca6a39d7e08c3f63bd3
Shaun <shaunl652@gmail.com>
2024-07-17 13:24:12 +0100

    Using the new rod design

--- geo_write.py
+++ geo_write.py
@@ -25,37 +25,37 @@ z0 = float(vals[1])
 # Distance from surface of the rod to its centre
 rod_rad = 0.635
 ECP_thick = 0.1
-Rod_Len = 15
+Rod_Len = 20
 gap = 0.5
 Move = Rod_Len -z0 + gap
 
 with open('TrapAC.scuffgeo','w') as f:
-    f.write(f'OBJECT Rod1\n   MESHFILE Rod.o.msh\n   DISPLACED {-(r0+rod_rad)} 0 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod1\n   MESHFILE RF_Upper.o.msh\n   DISPLACED 0 0 40\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod2\n   MESHFILE Rod.o.msh\n   DISPLACED {+(r0+rod_rad)} 0 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod2\n   MESHFILE RF_Lower.o.msh\n   DISPLACED 0 0 40\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod3\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {-(r0+rod_rad)} 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod3\n   MESHFILE RF_Upper.o.msh\n   DISPLACED 0 0 40\n   ROTATED 180 ABOUT 0 1 0\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod4\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {+(r0+rod_rad)} 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod4\n   MESHFILE RF_Lower.o.msh\n   DISPLACED 0 0 40\n   ROTATED 180 ABOUT 0 1 0\n   ROTATED 45 ABOUT 0 0 1\n   ENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Wehnelt\n MESHFILE Wehnelt.o.msh\n   DISPLACED 0 0 {z0}\nENDOBJECT\n')
+    f.write(f'OBJECT Wehnelt\n MESHFILE Rod.o.msh\n   DISPLACED 0 0 {32/2 + Rod_Len/2 -2} \nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Filament\n   MESHFILE Filament.o.msh\n   DISPLACED 0 0 {+z0+8}\nENDOBJECT\n')
+    #f.write(f'OBJECT Filament\n   MESHFILE Filament.o.msh\n   DISPLACED 0 0 {+z0+8}\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT End_Cap_Bot\n   MESHFILE Endcap_bottom.o.msh\n   DISPLACED 0 0 {-z0}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
+    f.write(f'OBJECT End_Cap_Bot\n   MESHFILE Endcap_bottom.o.msh\n   DISPLACED 0 0 {-(Rod_Len/2+1)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
 
     
 with open('TrapDC.scuffgeo','w') as f:
-    f.write(f'OBJECT Rod1\n   MESHFILE Rod.o.msh\n   DISPLACED {-(r0+rod_rad)} 0 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod1\n   MESHFILE RF_Upper.o.msh\n   DISPLACED 0 0 40\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod2\n   MESHFILE Rod.o.msh\n   DISPLACED {+(r0+rod_rad)} 0 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod2\n   MESHFILE RF_Lower.o.msh\n   DISPLACED 0 0 40\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod3\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {-(r0+rod_rad)} 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod3\n   MESHFILE RF_Upper.o.msh\n   DISPLACED 0 0 40\n   ROTATED 180 ABOUT 0 1 0\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Rod4\n   MESHFILE Rod.o.msh\n   DISPLACED 0 {+(r0+rod_rad)} 0\nENDOBJECT\n')
+    f.write(f'OBJECT Rod4\n   MESHFILE RF_Lower.o.msh\n   DISPLACED 0 0 40\n   ROTATED 180 ABOUT 0 1 0\n   ROTATED 45 ABOUT 0 0 1\n   ENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Wehnelt\n MESHFILE Wehnelt.o.msh\n   DISPLACED 0 0 {z0}\nENDOBJECT\n')
+    f.write(f'OBJECT Wehnelt\n MESHFILE Rod.o.msh\n   DISPLACED 0 0 {Rod_Len-3}\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT Filament\n   MESHFILE Filament.o.msh\n   DISPLACED 0 0 {+z0+8}\nENDOBJECT\n')
+    #f.write(f'OBJECT Filament\n   MESHFILE Filament.o.msh\n   DISPLACED 0 0 {+z0+8}\nENDOBJECT\n')
     f.write('\n')
-    f.write(f'OBJECT End_Cap_Bot\n   MESHFILE Endcap_bottom.o.msh\n   DISPLACED 0 0 {-z0}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
+    f.write(f'OBJECT End_Cap_Bot\n   MESHFILE Endcap_bottom.o.msh\n   DISPLACED 0 0 {-(Rod_Len+1)}\n   ROTATED 45 ABOUT 0 0 1\nENDOBJECT\n')
